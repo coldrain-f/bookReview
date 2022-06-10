6b1384532fc425d3c8fc6f323b5035ed895a7a6f
coldrain-f <intmain__@naver.com>
2022-06-10 19:07:28 +0900

    회원가입 API 구현 완료 - /api/signup ( Validation 처리X )

--- src/main/java/edu/bookreview/config/SecurityConfig.java
+++ src/main/java/edu/bookreview/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package edu.bookreview.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration // IoC
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
