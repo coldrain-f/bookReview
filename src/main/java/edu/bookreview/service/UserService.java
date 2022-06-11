1fde2119a35660610f3bc134143ad97324aa4d96
coldrain-f <intmain__@naver.com>
2022-06-11 20:33:13 +0900

    회원가입 Bean Validation, username, nickname 중복 예외처리 완료

--- src/main/java/edu/bookreview/service/UserService.java
+++ src/main/java/edu/bookreview/service/UserService.java
@@ -1,6 +1,8 @@
 package edu.bookreview.service;
 
 import edu.bookreview.entity.User;
+import edu.bookreview.exception.DuplicateNicknameException;
+import edu.bookreview.exception.DuplicateUsernameException;
 import edu.bookreview.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,16 @@ public class UserService {
 
     @Transactional
     public void signup(User user) {
+        boolean duplicatedUsername = userRepository.findByUsername(user.getUsername())
+                .isPresent();
+        if (duplicatedUsername)
+            throw new DuplicateUsernameException("The username already exists.");
+
+        boolean duplicatedNickname = userRepository.findByNickname(user.getNickname())
+                .isPresent();
+        if (duplicatedNickname)
+            throw new DuplicateNicknameException("The nickname already exists.");
+
         userRepository.save(user);
     }
 
