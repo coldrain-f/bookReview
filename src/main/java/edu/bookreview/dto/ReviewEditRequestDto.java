5dfe2354dfdc4fa9c70a65a7703180f90afdbf62
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-06-14 17:27:13 +0900

    게시글 수정, 삭제 기능 추가

---
+++ src/main/java/edu/bookreview/dto/ReviewEditRequestDto.java
@@ -0,0 +1,27 @@
+package edu.bookreview.dto;
+
+import edu.bookreview.entity.BookReview;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ReviewEditRequestDto {
+
+    private final String bookBuyUrl;
+    private final String title;
+    private final String content;
+    private final Integer rank;
+
+    public BookReview toEntity(BookReview bookReview){
+        return BookReview.builder()
+                .user(bookReview.getUser())
+                .bookBuyUrl(this.bookBuyUrl)
+                .title(this.title)
+                .content(this.content)
+                .bookImageUrl(bookReview.getBookImageUrl())
+                .likeCount(bookReview.getLikeCount())
+                .rank(this.rank)
+                .build();
+    }
+}
